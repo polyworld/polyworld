73fa56a31fde43f6f1d4d319c71f76a8b9f3d713
larryy <>
2005-09-05 22:37:13 +0000

    add support for brain recording; add FitStruct, Stat, And StatRecent structures

--- app/Simulation.h
+++ app/Simulation.h
@@ -30,7 +30,69 @@ class TSimulation;
 static const int MAXDOMAINS = 10;
 static const int MAXFITNESSITEMS = 5;
 
-//extern long TSimulation::fAge;
+struct FitStruct
+{
+	ulong	critterID;
+	float	fitness;
+	genome*	genes;
+};
+typedef struct FitStruct FitStruct;
+
+//===========================================================================
+// Stat
+//===========================================================================
+class Stat
+{
+public:
+	Stat()					{ reset(); }
+	~Stat()					{}
+	
+	float	mean()			{ if( !count ) return( 0.0 ); return( sum / count ); }
+	float	stddev()		{ if( !count ) return( 0.0 ); register double m = sum / count;  return( sqrt( sum2 / count  -  m * m ) ); }
+	float	min()			{ if( !count ) return( 0.0 ); return( mn ); }
+	float	max()			{ if( !count ) return( 0.0 ); return( mx ); }
+	void	add( float v )	{ sum += v; sum2 += v*v; count++; mn = v < mn ? v : mn; mx = v > mx ? v : mx; }
+	void	reset()			{ mn = FLT_MAX; mx = FLT_MIN; sum = sum2 = count = 0; }
+
+private:
+	float	mn;		// minimum
+	float	mx;		// maximum
+	double	sum;	// sum
+	double	sum2;	// sum of squares
+	unsigned long	count;	// count
+};
+
+//===========================================================================
+// StatRecent
+//===========================================================================
+class StatRecent
+{
+public:
+	StatRecent( unsigned int width = 1000 )	{ w = width; history = (float*) malloc( w * sizeof(*history) ); Q_CHECK_PTR( history); reset(); }
+	~StatRecent()							{ if( history ) free( history ); }
+	
+	float	mean()			{ if( !count ) return( 0.0 ); return( sum / count ); }
+	float	stddev()		{ if( !count ) return( 0.0 ); register double m = sum / count;  return( sqrt( sum2 / count  -  m * m ) ); }
+	float	min()			{ if( !count ) return( 0.0 ); if( needMin ) recomputeMin(); return( mn ); }
+	float	max()			{ if( !count ) return( 0.0 ); if( needMax ) recomputeMax(); return( mx ); }
+	void	add( float v )	{ if( count < w ) { sum += v; sum2 += v*v; mn = v < mn ? v : mn; mx = v > mx ? v : mx; history[index++] = v; count++; } else { if( index >= w ) index = 0; sum += v - history[index]; sum2 += v*v - history[index]*history[index]; if( v >= mx ) mx = v; else if( history[index] == mx ) needMax = true; if( v <= mn ) mn = v; else if( history[index] == mn ) needMin = true; history[index++] = v; } }
+	void	reset()			{ mn = FLT_MAX; mx = FLT_MIN; sum = sum2 = count = index = 0; needMin = needMax = false; }
+
+private:
+	float	mn;		// minimum
+	float	mx;		// maximum
+	double	sum;	// sum
+	double	sum2;	// sum of squares
+	unsigned long	count;	// count
+	unsigned int	w;		// width of data window considered current (controls roll-off)
+	float*	history;	// w recent samples
+	unsigned int	index;	// point in history[] at which next sample is to be inserted
+	bool	needMin;
+	bool	needMax;
+	
+	void	recomputeMin()	{ mn = FLT_MAX; for( unsigned int i = 0; i < w; i++ ) mn = history[i] < mn ? history[i] : mn; needMin = false; }
+	void	recomputeMax()	{ mx = FLT_MIN; for( unsigned int i = 0; i < w; i++ ) mx = history[i] > mx ? history[i] : mx; needMax = false; }
+};
 
 //===========================================================================
 // TSceneWindow
@@ -138,7 +200,7 @@ public:
 	void Step();
 	void Update();
 	
-	static long fAge;
+	static long fStep;
 	static short fOverHeadRank;
 	static critter* fMonitorCritter;
 	static double fFramesPerSecondOverall;
@@ -148,6 +210,11 @@ public:
 	static double fFramesPerSecondInstantaneous;
 	static double fSecondsPerFrameInstantaneous;
 	static double fTimeStart;
+
+	int fBestSoFarBrainAnatomyRecordFrequency;
+	int fBestSoFarBrainFunctionRecordFrequency;
+	bool fBrainAnatomyRecordAll;
+	bool fBrainFunctionRecordAll;
 
 private slots:
 	
@@ -182,9 +249,9 @@ private:
 	TTextStatusWindow* fTextStatusWindow;
 	
 	bool fPaused;
-	long fDelay;
-	long fDumpFrequency;
-	long fStatusFrequency;
+	int fDelay;
+	int fDumpFrequency;
+	int fStatusFrequency;
 	bool fLoadState;
 	bool inited;
 	
@@ -223,8 +290,7 @@ private:
 	float fCurrentMaxFitness[MAXFITNESSITEMS];
 	int fCurrentFittestCount;
 	int fNumberFit;
-	genome** fFittest;
-	float* fFitness;
+	FitStruct** fFittest;
 	long fFitness1Frequency;
 	long fFitness2Frequency;
 	short fFitI;
@@ -236,6 +302,7 @@ private:
 	float fEatThreshold;
 	
 	float fMaxFitness;
+	ulong fNumAverageFitness;
 	float fAverageFitness;
 	float fTotalFitness;
 	float fMateFitnessParameter;
@@ -301,6 +368,7 @@ private:
 	Stat fLifeSpanStats;
 	Stat fNeuronGroupCountStats;
 	Stat fCurrentNeuronGroupCountStats;
+	StatRecent fLifeSpanRecentStats;
 
 	float fSmiteFrac;
 	float fSmiteAgeFrac;
