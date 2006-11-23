429ccba2482ef8d91dc10ab87bd717eff6ca66b1
romanpoet <>
2006-11-23 06:06:46 +0000

    Added Lifespan

--- scripts/PopsizeandFoodoverTime.sh
+++ scripts/PopsizeandFoodoverTime.sh
@@ -27,8 +27,11 @@ do
 	CurNeurGroups=$(grep '^CurNeurGroups = ' "$statfile" | awk -F' ' '{ print $3 }')
 	CurNeurGroupsStdDev=$(grep '^CurNeurGroups = ' "$statfile" | awk -F' ' '{ print $5 }')
 
+	LifeSpan=$(grep '^LifeSpan = ' "$statfile" | awk -F' ' '{ print $3 }')
+	LifeSpanStdDev=$(grep '^LifeSpan = ' "$statfile" | awk -F' ' '{ print $5 }')
+
 	# StdDev tells us about the variance within a population.  StdErr tells us how representative the mean is.
 	CurNeurGroupsStdErr=$(echo "$CurNeurGroupsStdDev" | awk -v samplesize="$numcritters" '{ print $1 / sqrt(samplesize) }')
 
-	echo "$timestep	$numcritters	$amountfood	$CurNeurGroups	$CurNeurGroupsStdErr"
+	echo "$timestep	$numcritters	$amountfood	$CurNeurGroups	$CurNeurGroupsStdErr	$LifeSpan	$LifeSpanStdDev"
 done
