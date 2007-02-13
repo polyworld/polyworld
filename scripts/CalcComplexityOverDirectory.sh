1c5015864cf69b5356815b2e659b65377ae92a56
romanpoet <>
2007-02-13 04:23:47 +0000

    Added display of numneurons and num_timesteps_lived.

--- scripts/CalcComplexityOverDirectory.sh
+++ scripts/CalcComplexityOverDirectory.sh
@@ -24,12 +24,15 @@ echo "#CritterNum	All	Processing	Input"
 ls -1 | awk '$1 ~ /brainFunction/ && $1 !~ /incomplete_/ { print $0 }' | sort -n -t '_' -k2,4 | while read funcfile
 do
 	critternum=$(echo "$funcfile" | sed -e 's/^[0-9]*_//' | cut -f2 -d'_'  | cut -f1 -d'.')	# get the critter number out
+	# brainFunction 11111 61 24
+	numneurons=$(head -n 1 $funcfile | cut -d' ' -f3)
+	timesteps_in_lifetime=$(cat $funcfile | wc -l | sed -e 's/[ ]*//g' | awk -v numneur=${numneurons} '{ print int( $1 / numneur) }')
 
 	# now get the complexties.  They are in Columns: All / Processing / Input
 
 	line=$(${CALC_COMPLEXITY} $funcfile | awk -F'	' 'NR==1{  all=$2 } NR==2{ processing=$2 } NR==3{ input=$2 } END { print all "	" processing "	" input }')
 
-	echo "$critternum	$line	${directory}/${funcfile}"	# now echo the line
+	echo "$critternum	$line	${directory}/${funcfile}	$numneurons	$timesteps_in_lifetime" # now echo the line
 
 done
 
