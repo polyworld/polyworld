e1d51caf91d20460bcb3c735b92e82f764fe4635
romanpoet <>
2007-04-04 17:44:15 +0000

    now using the /polyworld/ environment variable

--- scripts/CalcComplexityOverDirectory.sh
+++ scripts/CalcComplexityOverDirectory.sh
@@ -4,7 +4,7 @@ CALC_COMPLEXITY="$pol/utils/CalcComplexity"
 # If the CalcComplexity command-line tool isn't compiled, compile it.
 if [ ! -x "$CALC_COMPLEXITY" ]
 then
-	g++ -lgsl -lgslcblas -lm -O3 '/polyworld/utils/CalcComplexity.cpp' -o ${CALC_COMPLEXITY}
+	g++ -lgsl -lgslcblas -lm -O3 "$pol/utils/CalcComplexity.cpp" -o ${CALC_COMPLEXITY}
 fi
 
 # Make sure we're taking an argument
@@ -20,13 +20,13 @@ directory=$(echo "${1}" | sed -e 's/\/*$//')
 # Go into the brainFunction directory (where the timesteps are)
 cd "$directory"
 
-echo "#CritterNum	All	Processing	Input"
+echo "#CritterNum	All	Processing	Input	numneurons	timesteps_in_lifetime"
 ls -1 | awk '$1 ~ /brainFunction/ && $1 !~ /incomplete_/ { print $0 }' | sort -n -t '_' -k2,4 | while read funcfile
 do
 	critternum=$(echo "$funcfile" | sed -e 's/^[0-9]*_//' | cut -f2 -d'_'  | cut -f1 -d'.')	# get the critter number out
 	# brainFunction 11111 61 24
 	numneurons=$(head -n 1 $funcfile | cut -d' ' -f3)
-	timesteps_in_lifetime=$(cat $funcfile | wc -l | sed -e 's/[ ]*//g' | awk -v numneur=${numneurons} '{ print int( $1 / numneur) }')
+	timesteps_in_lifetime=$(cat $funcfile | wc -l | tr -d ' ' | awk -v numneur=${numneurons} '{ print int( $1 / numneur) }')
 
 	# now get the complexties.  They are in Columns: All / Processing / Input
 
