0fe186d16951ffb7b57a80ffd918b09593c83eed
larryy <>
2010-01-04 22:49:22 +0000

    add gsl

--- tools/rancheck/rancheck.c
+++ tools/rancheck/rancheck.c
@@ -1,18 +1,1 @@
-// This program prints the first 10 random numbers from rand(), drand48(), and random()
-
-#include <stdio.h>
-#include <stdlib.h>
-
-int main( int argc, char** argv )
-{
-	int i;
-
-	srand( 42 );
-	srand48( 42 );
-	srandom( 42 );
-
-	for( i = 0; i < 10 ; i++ )
-		printf( "%d:  srand() = %10d,  drand48() = %06.4f,  random() = %10ld\n", i, rand(), drand48(), random() );
-
-	return( 0 );
-}
+// This program prints the first 10 random numbers from rand(), drand48(), random(), and gsl#include <stdio.h>#include <stdlib.h>#include <gsl/gsl_rng.h>#define SEED 42int main( int argc, char** argv ){	int i;	srand( SEED );	srand48( SEED );	srandom( SEED );	gsl_rng *gsl = gsl_rng_alloc( gsl_rng_mt19937 );	gsl_rng_set( gsl, SEED );	for( i = 0; i < 10 ; i++ )		printf( "%d:  srand = %10d,  drand48 = %06.4f,  random = %10ld,  gsl = %lf\n",				i,				rand(),				drand48(),				random(),				gsl_rng_uniform(gsl) );	return( 0 );}
