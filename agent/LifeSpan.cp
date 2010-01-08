403033f5f53664bdea137c70d492f0b96999aac3
larryy <>
2010-01-08 04:45:06 +0000

    part of Sean's motion complexity revision

---
+++ agent/LifeSpan.cp
@@ -0,0 +1,41 @@
+#include "LifeSpan.h"
+
+const char *LifeSpan::BR_NAMES[];
+const char *LifeSpan::DR_NAMES[];
+
+class ModuleInit
+{
+ public:
+	ModuleInit()
+	{
+#define BIRTH_TYPE(TYPE) LifeSpan::BR_NAMES[LifeSpan::BR_##TYPE] = #TYPE;
+		BIRTH_METAENUM();
+#undef BIRTH_TYPE
+
+#define DEATH_TYPE(TYPE) LifeSpan::DR_NAMES[LifeSpan::DR_##TYPE] = #TYPE;
+		DEATH_METAENUM();
+#undef DEATH_TYPE
+	}
+} module_init;
+
+
+LifeSpan::LifeSpan()
+{
+	birth.step = death.step = -1;
+	birth.reason = BR_INVALID;
+	death.reason = DR_INVALID;
+}
+
+void LifeSpan::set_birth( long step,
+						  BirthReason reason )
+{
+	birth.step = step;
+	birth.reason = reason;
+}
+
+void LifeSpan::set_death( long step,
+						  DeathReason reason )
+{
+	death.step = step;
+	death.reason = reason;
+}
---
+++ agent/LifeSpan.h
@@ -0,0 +1,73 @@
+#pragma once
+
+class LifeSpan
+{
+	// ---
+	// --- METAENUM Birth
+	// ---
+#define BIRTH_METAENUM(TYPE)					\
+	BIRTH_TYPE(INVALID)							\
+		BIRTH_TYPE(SIMINIT)						\
+		BIRTH_TYPE(CREATE)						\
+		BIRTH_TYPE(NATURAL)						\
+		BIRTH_TYPE(LOCKSTEP)
+
+	// ---
+	// --- METAENUM Death
+	// ---
+#define DEATH_METAENUM(TYPE)					\
+	DEATH_TYPE(INVALID)							\
+		DEATH_TYPE(SIMEND)						\
+		DEATH_TYPE(SMITE)						\
+		DEATH_TYPE(NATURAL)						\
+		DEATH_TYPE(LOCKSTEP)					\
+		DEATH_TYPE(RANDOM)
+
+ public:
+	// ---
+	// --- ENUM BirthReason
+	// ---
+#define BIRTH_TYPE(TYPE) BR_##TYPE,
+	enum BirthReason
+	{
+		BIRTH_METAENUM()
+		__BR_NTYPES
+	};
+#undef BIRTH_TYPE
+
+	// ---
+	// --- ENUM DeathReason
+	// ---
+#define DEATH_TYPE(TYPE) DR_##TYPE,
+	enum DeathReason
+	{
+		DEATH_METAENUM()
+		__DR_NTYPES
+	};
+#undef DEATH_TYPE
+
+	static const char *BR_NAMES[__BR_NTYPES];
+	static const char *DR_NAMES[__DR_NTYPES];
+
+ public:
+	LifeSpan();
+
+	void set_birth( long step,
+					BirthReason birthReason );
+				
+	void set_death( long step,
+					DeathReason deathReason );
+
+ public:
+	struct
+	{
+		BirthReason reason;
+		long step;
+	} birth;
+
+	struct
+	{
+		DeathReason reason;
+		long step;
+	} death;
+};
