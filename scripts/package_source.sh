42b832051878ba427490d4eb64f094f0368c8dd0
rikss <>
2010-08-12 01:06:15 +0000

    create scripts package_source.sh and run_history.sh -- used by pwfarm for zipping source for transport and maintaining source history in run dir.

---
+++ scripts/package_source.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+SRCDIRS="agent\
+         app\
+         brain\
+         complexity\
+         environment\
+         genome\
+         graphics\
+         tools\
+         ui\
+         utils"
+
+MISCDIRS="scripts\
+          objects"
+
+MISCFILES="Makefile"
+
+if [ $# != 1 ]; then
+    name=`basename $0`
+    echo "usage: $name output.zip"
+    echo
+    echo "Packages all the source code for the Polyworld source tree containing the script into output.zip"
+    echo
+    echo "Note: Can be executed from any working directory; will always package the tree containing it."
+    
+    exit 1
+fi
+
+function err()
+{
+    echo $* >&2; exit 1
+}
+function canonpath
+{
+    python -c "import os.path; print os.path.realpath('$1')"
+}
+
+if [ -z "$1" ]; then
+    err "Must specify output zip file"
+fi
+
+homedir=$(canonpath `dirname "$0"`/..)
+output=$(canonpath "$1")
+
+if [ "${output##*.}" != "zip" ]; then
+    err "Not a path with zip extension: $output"
+fi
+
+if [ -f "$output" ]; then
+    rm "$output" || exit 1
+fi
+
+cd "$homedir"
+
+zip -r "$output" $SRCDIRS $MISCDIRS $MISCFILES -x "*.pyc" -x "*/CVS/*" -x "*~"
---
+++ scripts/run_history.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+set -e
+
+if [ $# == 0 ]; then
+    name=`basename $0`
+    
+    echo "usage: $name src <run_dir> <failed> [<script>]"
+    exit 1
+fi
+
+function err()
+{
+    echo $* >&2; exit 1
+}
+
+mode="$1"
+if [ "$mode" != "src" ]; then
+    err "Unsupported mode: $mode"
+fi
+
+run_dir="$2"
+failed="3"
+script="$4"
+
+if [ ! -d "$run_dir" ]; then
+    err "Invalid run dir: $run_dir"
+fi
+
+if [ -z "$failed" ]; then
+    err "Missing 'failed' parm"
+fi
+
+if [ ! -z "$script" ] && [ ! -f "$script" ]; then
+    err "Invalid script: $script"
+fi
+
+
+src_dir="$run_dir/.history/src"
+mkdir -p $src_dir
+nentries=$(echo `ls $src_dir | wc -l`) # use echo in order to work around stupid \t insertion
+src_dir="$src_dir/$nentries"
+mkdir $src_dir
+
+
+if [ "$failed" == "true" ]; then
+    touch "$src_dir/FAILED"
+fi
+
+
+SCRIPTS=`dirname $0`
+
+$SCRIPTS/package_source.sh $src_dir/src.zip
+
+if [ ! -z "$script" ]; then
+    cp "$script" $src_dir/script
+fi
+
