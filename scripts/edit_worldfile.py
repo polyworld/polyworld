3c2832ff830b1e1c4a9e4edab10865d9babceb65
rikss <>
2010-09-19 00:16:03 +0000

    enhance edit_worldfile to support property wildcards -- allows managing arrays, like domain[*].xxx

--- scripts/edit_worldfile.py
+++ scripts/edit_worldfile.py
@@ -2,9 +2,14 @@
 
 from sys import argv, exit
 from os import path
+import re
 
 if len(argv) == 1:
+           ################################################################################
     print "usage:", path.basename(argv[0]), "input_file output_file (propname=value)*"
+    print
+    print "      Propname may include splat wildcard -- useful for arrays. If using splat,"
+    print "  be sure to put quotes around the argument so the shell doesn't screw it up."
     exit(1)
 
 orig = argv[1]
@@ -13,8 +18,11 @@ new = argv[2]
 edits = {}
 
 for edit in argv[3:]:
-    edit = edit.strip().split('=')
-    edits[edit[0]] = edit[1]
+    pattern, value = edit.strip().split('=')
+    pattern = '^' + pattern.replace('*', '.*').replace('[', '\[').replace(']', '\]') + '$'
+    edits[pattern] = value
+
+edits_notFound = list( edits.keys() )
 
 forig = open( orig )
 fnew = open( new, 'w' )
@@ -24,21 +32,22 @@ for line in forig:
     newline = line
 
     if( len(fields) >= 2 ):
-        try:
-            label = fields[1]
-            newval = edits[label]
-            newline = newval + '\t' + label + '\n'
-            del edits[label]
-        except KeyError:
-            # no edit for this label
-            pass
+        label = fields[1]
+
+        for pattern, value in edits.items():
+            if re.match( pattern, label ):
+                newline = value + '\t' + label + '\n'
+                try:
+                    edits_notFound.remove( pattern )
+                except:
+                    pass # edits can have multiple matches with patterns
     
     fnew.write( newline )
 
 forig.close()
 fnew.close()
 
-if len( edits ):
+if len( edits_notFound ):
     print "Couldn't find labels:", ', '.join( edits.keys() )
     exit( 1 )
 
