6cc016cf337de43835cb8849ee9f00cd7fa25838
rikss <>
2011-11-30 17:30:32 +0000

    make poly_lsrun.sh operate more like 'ls' than 'find'

--- scripts/farm/poly_lsrun.sh
+++ scripts/farm/poly_lsrun.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+if [ $# == 0 ]; then
+    echo "usage: $( basename $0 ) runid"
+    echo
+    echo "Checks if runid exists in either good or failed directories"
+
+    exit 1
+fi
+
+runid=$1
+
 PWHOSTNUMBERS=`cat ~/polyworld_pwfarm/etc/pwhostnumbers`
 USER=`cat ~/polyworld_pwfarm/etc/pwuser`
 
@@ -9,10 +19,13 @@ for x in $PWHOSTNUMBERS; do
     eval pwhost=\$$pwhostname
 
     echo --- $pwhostname ---
-    ssh -l $USER $pwhost '\
-	find ~/polyworld_pwfarm/runs -name "normalized.wf" | \
-	while read x; do dirname $x; done; \
-	find ~/polyworld_pwfarm/runs -name "worldfile" | \
-	while read x; do dirname $x; done \'
+    ssh -l $USER $pwhost "\
+        cd
+        if [ -e \"polyworld_pwfarm/runs/good/$runid\" ]; then
+            file \"polyworld_pwfarm/runs/good/$runid\"
+        fi
+        if [ -e \"polyworld_pwfarm/runs/failed/$runid\" ]; then
+            file \"polyworld_pwfarm/runs/failed/$runid\"
+        fi"
 
 done
