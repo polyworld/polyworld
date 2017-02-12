8291e6ffbbf57406647ca4c1d95eca94193640f8
Steven Williams <stevencwilliams@gmail.com>
2017-02-12 13:23:25 -0500

    Add truncation option to time series converter

--- scripts/bf2ts.py
+++ scripts/bf2ts.py
@@ -9,13 +9,24 @@ import sys
 def parseArgs():
     parser = argparse.ArgumentParser(description = "Converts brain function files to time series.")
     parser.add_argument("run", metavar = "RUN", help = "run directory")
-    parser.add_argument("--steps", metavar = "STEPS", type = int, help = "number of timesteps")
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument("--steps", metavar = "STEPS", type = int, help = "number of timesteps")
+    group.add_argument("--max-steps", metavar = "MAX_STEPS", type = int, help = "maximum number of timesteps")
     return parser.parse_args()
+
+def writeStep(agent, step):
+    for neuron in range(agent.func.num_neurons - 1):
+        if neuron > 0:
+            sys.stdout.write(" ")
+        sys.stdout.write("{0}".format(agent.func.acts[neuron, step]))
+    sys.stdout.write("\n")
 
 args = parseArgs()
 sys.stdout.write("# BEGIN ARGUMENTS\n")
 if args.steps is not None:
     sys.stdout.write("steps = {0}\n".format(args.steps))
+elif args.max_steps is not None:
+    sys.stdout.write("max-steps = {0}\n".format(args.max_steps))
 sys.stdout.write("# END ARGUMENTS\n")
 funcPath = os.path.join(args.run, "brain", "function")
 anatPath = os.path.join(args.run, "brain", "anatomy")
@@ -36,19 +47,27 @@ for funcFileName in os.listdir(funcPath):
         sys.stdout.write("\n")
     sys.stdout.write("# END SYNAPSES\n")
     sys.stdout.write("# BEGIN ENSEMBLE\n")
-    stepCount = 0
-    while True:
+    if args.max_steps is None:
+        if args.steps is None:
+            stepCountMax = agent.func.timesteps_lived
+        else:
+            stepCountMax = args.steps
+        stepCount = 0
+        while stepCount < stepCountMax:
+            sys.stdout.write("# BEGIN TIME SERIES\n")
+            for step in range(agent.func.timesteps_lived):
+                writeStep(agent, step)
+                stepCount += 1
+                if stepCount == stepCountMax:
+                    break
+            sys.stdout.write("# END TIME SERIES\n")
+    else:
+        if agent.func.timesteps_lived <= args.max_steps:
+            steps = range(agent.func.timesteps_lived)
+        else:
+            steps = range(agent.func.timesteps_lived - args.max_steps, agent.func.timesteps_lived)
         sys.stdout.write("# BEGIN TIME SERIES\n")
-        for step in range(agent.func.timesteps_lived):
-            for neuron in range(agent.func.num_neurons - 1):
-                if neuron > 0:
-                    sys.stdout.write(" ")
-                sys.stdout.write("{0}".format(agent.func.acts[neuron, step]))
-            sys.stdout.write("\n")
-            stepCount += 1
-            if args.steps is not None and stepCount == args.steps:
-                break
+        for step in steps:
+            writeStep(agent, step)
         sys.stdout.write("# END TIME SERIES\n")
-        if args.steps is None or stepCount == args.steps:
-            break
     sys.stdout.write("# END ENSEMBLE\n")
