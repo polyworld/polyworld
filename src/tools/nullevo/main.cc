9a1c69a3c06453afb1a5aa6b114e8140c2b0871a
Steven Williams <stevencwilliams@gmail.com>
2018-10-12 13:18:00 -0400

    Store genomes in vector

--- src/tools/nullevo/main.cc
+++ src/tools/nullevo/main.cc
@@ -1,7 +1,6 @@
 #include <assert.h>
 #include <iostream>
 #include <iterator>
-#include <map>
 #include <math.h>
 #include <stdlib.h>
 #include <string>
@@ -14,18 +13,17 @@
 #include "utils/analysis.h"
 #include "utils/misc.h"
 
-#define COMMA ,
+#define BATCH 100
 
 struct Args {
     std::string run;
     int agents;
-    int births;
+    int steps;
     std::string path;
 };
 
 void printUsage(int, char**);
 bool tryParseArgs(int, char**, Args&);
-std::map<int, genome::Genome*>::iterator pick(std::map<int, genome::Genome*>&);
 void update(std::vector<std::vector<int> >&, genome::Genome*, int);
 double getDistance(const std::vector<std::vector<int> >&, int);
 void logGenome(const std::string&, genome::Genome*);
@@ -37,49 +35,52 @@ int main(int argc, char** argv) {
         return 1;
     }
     analysis::initialize(args.run);
-    std::map<int, genome::Genome*> genomes;
+    std::vector<genome::Genome*> genomes;
     int geneCount = genome::GenomeUtil::schema->getMutableSize();
     std::vector<std::vector<int> > distribution(geneCount, std::vector<int>(256));
     genome::Genome* seed = analysis::getGenome(args.run, 1);
     for (int agent = 1; agent <= args.agents; agent++) {
         genome::Genome* genome = genome::GenomeUtil::createGenome();
         genome->copyFrom(seed);
-        genomes[agent] = genome;
+        genomes.push_back(genome);
     }
     update(distribution, seed, args.agents);
     std::cout << 0 << " " << getDistance(distribution, args.agents) << std::endl;
-    for (int birth = 1; birth <= args.births; birth++) {
-        int agent = args.agents + birth;
-        std::map<int, genome::Genome*>::iterator parent1 = pick(genomes);
-        std::map<int, genome::Genome*>::iterator parent2;
+    for (int step = 1; step <= args.steps; step++) {
+        int parent1 = (int)rrand(0, genomes.size());
+        int parent2;
         do {
-            parent2 = pick(genomes);
-        } while (parent2->first == parent1->first);
-        genome::Genome* genome = genome::GenomeUtil::createGenome();
-        genome->crossover(parent1->second, parent2->second, true);
-        genomes[agent] = genome;
-        update(distribution, genome, 1);
-        std::map<int, genome::Genome*>::iterator genomesIter = pick(genomes);
-        genomes.erase(genomesIter->first);
-        update(distribution, genomesIter->second, -1);
-        delete genomesIter->second;
-        std::cout << birth << " " << getDistance(distribution, args.agents) << std::endl;
+            parent2 = (int)rrand(0, genomes.size());
+        } while (parent2 == parent1);
+        genome::Genome* birth = genome::GenomeUtil::createGenome();
+        birth->crossover(genomes[parent1], genomes[parent2], true);
+        int child = (int)rrand(0, genomes.size());
+        genome::Genome* death = genomes[child];
+        genomes[child] = birth;
+        update(distribution, birth, 1);
+        update(distribution, death, -1);
+        delete death;
+        if (step % BATCH == 0) {
+            std::cout << step << " " << getDistance(distribution, args.agents) << std::endl;
+        }
     }
     makeDirs(args.path);
-    itfor(std::map<int COMMA genome::Genome*>, genomes, genomesIter) {
-        logGenome(args.path + "/genome_" + std::to_string(genomesIter->first) + ".txt", genomesIter->second);
+    for (int agent = 1; agent <= args.agents; agent++) {
+        genome::Genome* genome = genomes[agent - 1];
+        logGenome(args.path + "/genome_" + std::to_string(agent) + ".txt", genomes[agent - 1]);
+        delete genome;
     }
     return 0;
 }
 
 void printUsage(int argc, char** argv) {
-    std::cerr << "Usage: " << argv[0] << " RUN AGENTS BIRTHS PATH" << std::endl;
+    std::cerr << "Usage: " << argv[0] << " RUN AGENTS STEPS PATH" << std::endl;
     std::cerr << std::endl;
     std::cerr << "Generates selection-agnostic agents assuming steady-state conditions." << std::endl;
     std::cerr << std::endl;
     std::cerr << "  RUN     Run directory" << std::endl;
     std::cerr << "  AGENTS  Number of agents in the population" << std::endl;
-    std::cerr << "  BIRTHS  Number of births to simulate" << std::endl;
+    std::cerr << "  STEPS   Number of steps to simulate" << std::endl;
     std::cerr << "  PATH    Logged genome directory" << std::endl;
 }
 
@@ -89,7 +90,7 @@ bool tryParseArgs(int argc, char** argv, Args& args) {
     }
     std::string run;
     int agents;
-    int births;
+    int steps;
     std::string path;
     try {
         int argi = 1;
@@ -102,8 +103,8 @@ bool tryParseArgs(int argc, char** argv, Args& args) {
             return false;
         }
         agents = 256 * (int)ceil(agents / 256.0);
-        births = atoi(argv[argi++]);
-        if (births < 0) {
+        steps = atoi(argv[argi++]);
+        if (steps < 0) {
             return false;
         }
         path = std::string(argv[argi++]);
@@ -115,15 +116,9 @@ bool tryParseArgs(int argc, char** argv, Args& args) {
     }
     args.run = run;
     args.agents = agents;
-    args.births = births;
+    args.steps = steps;
     args.path = path;
     return true;
-}
-
-std::map<int, genome::Genome*>::iterator pick(std::map<int, genome::Genome*>& genomes) {
-    std::map<int, genome::Genome*>::iterator genome = genomes.begin();
-    std::advance(genome, (int)rrand(0, genomes.size()));
-    return genome;
 }
 
 void update(std::vector<std::vector<int> >& distribution, genome::Genome* genome, int increment) {
